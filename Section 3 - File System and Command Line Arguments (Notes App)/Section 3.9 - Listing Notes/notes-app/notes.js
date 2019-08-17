a03d117d304c6c22ea90bd38e4750559cca15b41
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-08-17 22:23:35 +0100

    Section 3 - File System and Command Line Arguments (Notes App) - 3.9. Listing Notes.

---
+++ Section 3 - File System and Command Line Arguments (Notes App)/Section 3.9 - Listing Notes/notes-app/notes.js
@@ -0,0 +1,84 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Read Notes Function:
+const getNotes = () => 'Your notes...'
+
+
+// Add Notes Function:
+const addNote = (title, body) => {
+   const notes = loadNotes()
+
+   // Check for Duplicates
+   const duplicateNotes = notes.filter((note) => note.title === title)
+
+   if (duplicateNotes.length === 0) {
+      // Append Objects and properties to notes array.
+      notes.push({
+         title: title, 
+         body: body
+      })
+
+      // console.log(notes)      // Testing to see the notes object
+      saveNotes(notes)           // Save the notes
+      console.log(chalk.green.inverse('New note added!'))
+   } else {
+      console.log(chalk.red.inverse('Note title is taken!'))
+   }
+}
+
+
+// List Notes Function:
+const listNotes = () => {
+   console.log(chalk.inverse('Your Notes:'))
+
+   const notes = loadNotes()
+
+   notes.forEach((note) => console.log(note.title))
+}
+
+
+// Remove Notes Function:
+const removeNote = (title) => {
+   // console.log(title)      // Testing to see the title returned to delete
+   const notes = loadNotes()
+
+   // Create new array keeping all objects where the titles do not match
+   const notesToKeep = notes.filter((note) => note.title !== title)
+
+   // Log useful message to the user
+   if (notes.length > notesToKeep.length) {
+      console.log(chalk.green.inverse('Note Removed!'))
+      saveNotes(notesToKeep)     // Save updated notes array
+   } else {
+      console.log(chalk.red.inverse('No Note Found!'))
+   }
+}
+
+
+// Loading Notes Function:
+const loadNotes = () => {
+   try {
+      const dataBuffer = fs.readFileSync('notes.json')
+      const dataJSON = dataBuffer.toString()
+      return JSON.parse(dataJSON)
+   } catch(err) {
+      return []
+   }
+}
+
+
+// Saving Notes Function:
+const saveNotes = (notes) => {
+   const dataJSON = JSON.stringify(notes)
+   fs.writeFileSync('notes.json', dataJSON)
+}
+
+
+// Exporting Functions:
+module.exports = {
+   getNotes: getNotes,
+   addNote: addNote,
+   removeNote: removeNote,
+   listNotes: listNotes
+}
