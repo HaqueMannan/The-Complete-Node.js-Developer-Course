db844e19d3ddfe3f91086dbc75d9d41e7b8cd654
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-09-07 22:49:45 +0100

    Section 5 - Asynchronous Node (Weather App) - 5.7. Callback Abstraction.

---
+++ Section 5 - Asynchronous Node.js (Weather App)/Section 5.7 - Callstack Abstraction/weather-app/app.js
@@ -0,0 +1,12 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+geocode('London', (error, data) => {
+   console.log('Error:', error)
+   console.log('Data:', data)
+})
+
+forecast(37.8267, -122.4233, (error, data) => {
+   console.log('Error:', error),
+   console.log('Data:', data)
+})
