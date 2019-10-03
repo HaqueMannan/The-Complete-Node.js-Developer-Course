d18e55bdf5e36a7566193997a03331a3fa0db26d
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-10-03 23:33:17 +0100

    Section 10 - API Authentication and Security (Task App) - 10.7. Logging Out.

--- Section 10 - API Authentication and Security (Task App)/10.6 - Accepting Authentication Tokens/task-manager/src/middleware/auth.js
+++ Section 10 - API Authentication and Security (Task App)/10.6 - Accepting Authentication Tokens/task-manager/src/middleware/auth.js
@@ -6,7 +6,7 @@ const auth = async (req, res, next) => {
       const token = req.header('Authorization').replace('Bearer ', '')
       // console.log(token)
       const decoded = jwt.verify(token, 'newjsonwebtokensecret')
-      const user = await User.findOne({ _id: decoded._id })
+      const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
       if(!user) {
          throw new Error()
