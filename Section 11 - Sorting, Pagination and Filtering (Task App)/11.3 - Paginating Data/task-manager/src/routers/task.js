e3c10a939809fbfe2136c45bea3be8f74cc7644f
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-10-05 13:16:48 +0100

    Section 11 - Sorting, Pagination and Filtering (Task App) - 11.3. Paginating Data.

---
+++ Section 11 - Sorting, Pagination and Filtering (Task App)/11.3 - Paginating Data/task-manager/src/routers/task.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const Task = require('../models/task')
+const auth = require('../middleware/auth')
+
+// Create new router
+const router = new express.Router()
+
+// Resource Reading Endpoints
+router.get('/tasks', auth, async (req, res) => {
+   const match = {}
+
+   if(req.query.completed) {
+      match.completed = req.query.completed === 'true'
+   }
+
+   try {
+      await req.user.populate({
+         path:'tasks',
+         match,
+         options: {
+            limit: parseInt(req.query.limit),
+            skip: parseInt(req.query.skip)
+         }
+      }).execPopulate()
+      res.send(req.user.tasks)
+   } catch (error) {
+      res.status(500).send()
+   }
+})
+
+router.get(('/tasks/:id'), auth, async (req, res) => {
+   const _id = req.params.id
+
+   try {
+      const task = await Task.findOne({ _id, owner: req.user._id })
+
+      if(!task) {
+         return res.status(404).send()
+      }
+
+      res.send(task)
+   } catch (error) {
+      res.status(500).send()
+   }
+})
+
+// Resource Creating Endpoints
+router.post('/tasks', auth, async (req, res) => {
+   // const task = new Task(req.body)
+   const task = new Task({
+      ...req.body,
+      owner: req.user._id
+   })
+
+   try {
+      await task.save()
+      res.status(201).send(task)
+   } catch (error) {
+      res.status(400).send(error)
+   }
+})
+
+// Resource Updating Endpoints
+router.patch('/tasks/:id', auth, async (req, res) => {
+   const updates = Object.keys(req.body)
+   const allowedUpdates = ['description', 'completed']
+   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+   if(!isValidOperation) {
+      return res.status(400).send({ error: 'Invalid updates!' })
+   }
+
+   try {
+      const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+
+      if(!task) {
+         return res.status(404).send()
+      }
+
+      updates.forEach((update) => task[update] = req.body[update])
+      await task.save()
+
+      res.send(task)
+   } catch (error) {
+      res.status(400).send(error)
+   }
+})
+
+// Resource Deleting Endpoints
+router.delete('/tasks/:id', auth, async (req, res) => {
+   try {
+      const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+
+      if(!task) {
+         return res.status(404).send()
+      }
+
+      res.send(task)
+   } catch (error) {
+      res.status(500).send(error)
+   }
+})
+
+// Export router
+module.exports = router
